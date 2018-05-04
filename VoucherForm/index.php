6abec36004740d29e740d6c3edbd079a5075254e
Lay Nadji <laynadji@gmail.com>
2018-05-04 17:14:53 +0700

    test
    test

---
+++ VoucherForm/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<!-- Viewport-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!-- fontawesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+
+
+	 </head>
+
+<body>
+
+<div class="container">
+
+<!-- Logo -->
+		<div class="logo">
+				<img src="https://www.levelsclub.com/wp-content/uploads/2017/10/logo.png">
+		</div>
+<!-- Video  -->
+		<div class="Video">
+				<iframe width="100%" height="500" src="https://www.youtube.com/embed/1a69ofqyAd0?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</div>
+
+<h1>Please complet the form to get your voucher</h1>
+<!-- Form  -->
+<form name="htmlform" method="post" action="html_form_send.php">
+	<table >
+		</tr>
+			<tr>
+
+ 				<td valign="top">
+ 			  		<input  type="text" name="first_name" maxlength="50" size="30" Placeholder="First Name">
+ 				</td>
+			</tr>
+
+			<tr>
+ 				<td valign="top">
+ 			 		<input  type="text" name="last_name" maxlength="50" size="30" Placeholder="Last Name">
+ 				</td>
+			</tr>
+
+			<tr>
+				<td valign="top">
+  			 		<input  type="text" name="email" maxlength="80" size="30" Placeholder="Email Address">
+ 			</td>
+			</tr>
+
+			<tr>
+				<td>
+					<button type="submit" class="btn btn-success"><i class="fas fa-ticket-alt"></i>  Get your voucher now </button>
+				</td>
+			</tr>
+</table>
+</form>
+
+<!-- Social  -->
+		<ul class="icons">
+
+			<li><a href="#" class="twitter_icon"><i class="fab fa-twitter"></i></span></a></li>
+			<li><a href="fb://profile/471209556256180" class="fb_icon"><i class="fab fa-facebook-f"></i></span></a></li>
+			<li><a href="#" class="Insta_icon"><i class="fab fa-instagram"></i></span></a></li>
+			<li><a href="#" class="tripadvisor_icon"><i class="fab fa-tripadvisor"></i></span></a></li>
+			<li><a href="#" class="Line_icon"><i class="fab fa-line"></i></span></a></li>
+						
+		</ul>
+
+<hr>
+
+<!-- Info  -->
+<div class="Info">
+
+<h1> Contact us </h1>
+
+<p><strong>ADDRESS : </strong>6th Floor of the Aloft Hotel</p>
+<p>No.35, Sukhumvit Soi 11, Sukhumvit Rd. (เลขที่ 35, ชั้น 6, ซ.สุขุมวิท 11) Bangkok, Thailand </p>
+<a href="tel:+660823083246"><strong>PHONE :</strong>(+66) (0) 82-308-3246</a>
+<a href="info@levelsclub.com"><strong>EMAIL : </strong>info@levelsclub.com</a>
+</div>
+
+<!-- Copyright  -->
+<div class="copyright">
+    <span>Ⓒ Levels Club  2018 All Rights Reserved</span>
+    </div>
+</div>
+</body>
+</html>
