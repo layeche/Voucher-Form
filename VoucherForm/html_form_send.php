ad633770c4bbe8408ca79769d9408d09e6f2ea54
ali <nadji-ali@hotmail.fr>
2018-05-07 14:06:30 +0200

    added default php mailer

--- VoucherForm/html_form_send.php
+++ VoucherForm/html_form_send.php
@@ -38,7 +38,7 @@ try {
 }
 
 /***Envoi mail*/
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+/*$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
@@ -68,3 +68,38 @@ try {
 } catch (Exception $e) {
     echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
+*/
+/*essai dabord avec ça*/
+$to = "somebody@example.com, somebodyelse@example.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>$token</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
